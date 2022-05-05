e8d868aba4fa105ea321061d36c5217771b1b679
Sean Broughton <seanbroughton03@gmail.com>
2022-05-05 15:58:22 -0400

    Set up code to connect the Idle Animation to Player Movement, created code to stop the player in the same direction they are facing, fixed bug where player will constantly switch idle animations by adjusting the greater or less than tabs in the animator, fixed bug where player animation would flicker when moving by adjusting animation transition times to 0, and created a variable for moveSpeed.

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -7,6 +7,7 @@ public class PlayerMain : MonoBehaviour
     
     [SerializeField] Rigidbody2D playerPhysics;
     [SerializeField] Animator basePlayerAnimator;
+    [SerializeField] int moveSpeed = 1;
 
     void Start()
     {
@@ -24,5 +25,12 @@ public class PlayerMain : MonoBehaviour
     // Connects Player Movement to the the Animator
         basePlayerAnimator.SetFloat("movementX", playerPhysics.velocity.x);
         basePlayerAnimator.SetFloat("movementY", playerPhysics.velocity.y);
+    // Controls the Idle Animation to face the right direction when the player stops
+        if(horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            basePlayerAnimator.SetFloat("LastX", horizontalMovement);
+            basePlayerAnimator.SetFloat("LastY", verticalMovement);
+        }
+
     }
 }
