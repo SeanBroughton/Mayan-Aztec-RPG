15ab0989fcd91b29e9b408a5e8937e96d876e72e
Sean Broughton <seanbroughton03@gmail.com>
2022-05-05 14:14:42 -0400

    Added a PlayerPhysics Tab to the Base Player , learned more about  Private, Public, and Serialized Fields options, and Organized the Folders into Sections

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+    
+    [SerializeField] Rigidbody2D playerPhysics;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
